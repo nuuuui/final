3b2345ae175457ea683b97740694c8961fdc5497
nuuuui <148730412+nuuuui@users.noreply.github.com>
2024-01-19 15:17:43 +0900

    a

--- src/delete-input.php
+++ src/delete-input.php
@@ -2,21 +2,26 @@
 <?php require 'menu.php'; ?>
 <?php require 'db-connect.php'; ?>
 
+    
 <?php
+    echo '<table>';
+    echo '<tr><th>お名前</th><th>　飼い主</th><th>　詳細</th><th>　ジャンルID</th><th>　種類</th></tr>';
     $pdo=new PDO($connect, USER, PASS);
     foreach ($pdo->query('select * from pet inner join genre where pet.genre_id = genre.genre_id') as $row) {
         echo '<tr>';
-        echo '<td>', $row['owner'], '</td>';
         echo '<td>', $row['name'], '</td>';
-        echo '<td>', $row['shosai'], '</td>';
-        echo '<td>', $row['genre_name'], '</td>';
-        echo '<td>', $row['animal_name'], '</td>';
-        echo '<td>';
+        echo '<td>　', $row['owner'], '</td>';
+        echo '<td>　', $row['shosai'], '</td>';
+        echo '<td>　', $row['genre_name'], '</td>';
+        echo '<td>　', $row['animal_name'], '</td>';
+        echo '<td>　';
         echo '<a href="delete-output.php?pet_id=', $row['pet_id'], '">削除</a>';
         echo '<td>';
         echo '</tr>';
         echo "\n";
     }
+    echo '</table>';
 ?>
+
 
 <?php require 'footer.php'; ?>
--- src/header.php
+++ src/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>PHP Sample Programs</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+<title>最終課題</title>
 </head>
 <body>
